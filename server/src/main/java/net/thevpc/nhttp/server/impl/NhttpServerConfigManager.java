ef5a5dd9fd7e3f3d46755a830eb1cb062d8a3096
thevpc <taha.bensalah@gmail.com>
2025-11-30 21:09:40 +0100

    [CHANGE] update API for v0.8.9

--- server/src/main/java/net/thevpc/nhttp/server/impl/NhttpServerConfigManager.java
+++ server/src/main/java/net/thevpc/nhttp/server/impl/NhttpServerConfigManager.java
@@ -40,7 +40,7 @@ public class NhttpServerConfigManager implements NBlankable {
     }
 
     public void tryReload() {
-        Instant otherInstant = configFile.lastModifiedInstant();
+        Instant otherInstant = configFile.getLastModifiedInstant();
         if (lastLoaded == null || otherInstant == null || lastLoaded.isBefore(otherInstant)) {
             reload();
         }
@@ -109,7 +109,7 @@ public class NhttpServerConfigManager implements NBlankable {
                         parseOneConfItem(object);
                     }
                 }
-                lastLoaded = configFile.lastModifiedInstant();
+                lastLoaded = configFile.getLastModifiedInstant();
             } else {
                 lastLoaded=null;
                 logger.info(NMsg.ofC("resetting config (config file not found %s)", configFile.toAbsolute().toString()));
